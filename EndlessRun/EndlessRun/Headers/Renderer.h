cfc4ddb331e4ddc6fe4a7187ce117112caa0122f
Damian <Damian@LAPTOP-5HGI8R3J>
2019-03-17 13:05:34 +0100

    Working

--- EndlessRun/EndlessRun/Headers/Renderer.h
+++ EndlessRun/EndlessRun/Headers/Renderer.h
@@ -8,9 +8,7 @@
 #include "Shader.h"
 
 #define ASSERT(x) if (!(x)) __debugbreak();
-#define GLCall(x) GLClearError();\
-		x;\
-		ASSERT(GLLogCall(#x, __FILE__, __LINE__))
+#define GLCall(x) GLClearError(); x; ASSERT(GLLogCall(#x, __FILE__, __LINE__))
 
 void GLClearError();
 
--- EndlessRun/EndlessRun/Headers/VertexBufferLayout.h
+++ EndlessRun/EndlessRun/Headers/VertexBufferLayout.h
@@ -3,7 +3,6 @@
 #pragma once
 
 #include "libs.h"
-#include "Assert.h"
 
 #include <vector>
 
